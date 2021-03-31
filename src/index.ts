e6b5a14b5ac16cb4378a6ddbbfc6d0ff7745353e
Adrien Martin <adrien.v.martin@gmail.com>
2021-03-31 13:34:49 -0600

    Import new classes

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import { MatchReader } from './MatchReader';
 import { CsvFileReader } from './CsvFileReader';
+import { ConsoleReport } from './reportTargets/ConsoleReport';
+import { WinsAnalysis } from './analyzers/WinsAnalysis';
+import { Summary } from './Summary';
 
 // Create an object that satisfies the DataReader interface
 const csvFileReader = new CsvFileReader('football.csv');
@@ -8,3 +11,10 @@ const csvFileReader = new CsvFileReader('football.csv');
 // the DataReader interface
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
+
+const summary = new Summary(
+  new WinsAnalysis('Man United'),
+  new ConsoleReport()
+);
+
+summary.buildAndPrintReport(matchReader.matches);
