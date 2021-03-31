9b70522bfcba54b7cb472505f240e56b88863ac9
Adrien Martin <adrien.v.martin@gmail.com>
2021-03-31 13:32:37 -0600

    Delete unneeded code from index

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import { MatchReader } from './MatchReader';
 import { CsvFileReader } from './CsvFileReader';
-import { MatchResult } from './MatchResult';
 
 // Create an object that satisfies the DataReader interface
 const csvFileReader = new CsvFileReader('football.csv');
@@ -9,15 +8,3 @@ const csvFileReader = new CsvFileReader('football.csv');
 // the DataReader interface
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
-
-let manUnitedWins = 0;
-
-for (let match of matchReader.matches) {
-  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
-    manUnitedWins++;
-  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
-    manUnitedWins++;
-  }
-}
-
-console.log(`Man United won ${manUnitedWins} games`);
